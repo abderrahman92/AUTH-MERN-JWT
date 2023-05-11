bb429d867ced9e3a42a3601a5b28104fd8699421
abderrahman ben salah <bensalahabdelrrahman1@gmail.com>
2023-05-11 13:04:49 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+// db
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("db connected");
+
+    const PORT = 8000;
+    app.listen(PORT, () => {
+      console.log("server is active");
+    });
+  }
+);
+
+// mw
+app.use(express.json());
+express.urlencoded({ extended: true });
+app.use(cookieParser());
+
+// routes
+app.use(userRoutes);
